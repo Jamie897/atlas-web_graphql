44353068a7f46a18029b53d0b541ae812c6ca2f1
Jamie897 <jamiedanielle456@gmail.com>
2024-03-12 16:49:21 -0500

    .

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema'); // Import the schema from schema.js
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// connect MongoDB Atlas database
+const mongoDBAtlasUri = 'mongodb+srv://JamieToman:Password@atlascluster.tqd66ql.mongodb.net/?retryWrites=true&w=majority';
 
 app.use('/graphql', graphqlHTTP({
   schema, // Pass the imported schema here
