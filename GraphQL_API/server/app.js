cc513a2a7ca3f8bd7c928add390ca574c2fa0b6f
Jamie897 <jamiedanielle456@gmail.com>
2024-03-12 16:07:29 -0500

    .

---
+++ GraphQL_API/server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema'); // Import the schema from schema.js
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema, // Pass the imported schema here
+}));
+
+app.listen(4000, () => {
+  console.log('now listening for request on port 4000');
+});
